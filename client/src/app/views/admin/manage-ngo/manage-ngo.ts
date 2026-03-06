8dbf47fe51f46d45790bee894a6e1c899bed40fa
QUJIAWEI-SS <b2400595@helplive.edu.my>
2026-03-06 20:31:30 +0800

    after add admin and employee folder

---
+++ client/src/app/views/admin/manage-ngo/manage-ngo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-ngo',
+  imports: [],
+  templateUrl: './manage-ngo.html',
+  styleUrl: './manage-ngo.css',
+})
+export class ManageNgo {}
