61e52f813fca3fef31606a6b2a01bcd8ef4da176
koritsushi <lyuan.2581@gmail.com>
2026-03-11 02:13:04 +0800

    (fix): fix notificationService typo

--- client/src/app/services/notification.service.ts
+++ client/src/app/services/notification.service.ts
@@ -5,13 +5,13 @@ import { Notification } from '../models/notification.model';
 @Injectable({
   providedIn: 'root'
 })
-export class NotiifcationService {
+export class NotificationService {
     notifications$ = signal<Notification[]>([]);
     notification$ = signal<Notification>({} as Notification);
     
     constructor(private httpClient: HttpClient) { }
 
-    private refreshRegistration() {
+    private refreshNotification() {
         this.httpClient.get<Notification[]>(`/notification`)
         .subscribe(notification => {
             this.notifications$.set(notification);
@@ -19,26 +19,26 @@ export class NotiifcationService {
     }
 
     getNotifications() {
-        this.refreshRegistration();
+        this.refreshNotification();
         return this.notifications$();
     }
 
-    getRegistration(id: string) {
+    getNotification(id: string) {
         this.httpClient.get<Notification>(`/notification/${id}`).subscribe(notification => {
         this.notification$.set(notification);
         return this.notifications$();
         });
     }
 
-    createRegistration(notification: Notification) {
+    createNotification(notification: Notification) {
         return this.httpClient.post(`/registration`, notification, { responseType: 'text' });
     }
 
-    updateRegistration(id: string, notification: Notification) {
+    updateNotification(id: string, notification: Notification) {
         return this.httpClient.put(`/registration/${id}`, notification, { responseType: 'text' });
     }
 
-    deleteRegistration(id: string) {
+    deleteNotification(id: string) {
         return this.httpClient.delete(`/notification/${id}`, { responseType: 'text' });
     }
 }
