a6285e3280cf12137a68b286ccb46d3558bcb885
koritsushi <lyuan.2581@gmail.com>
2026-03-11 21:25:01 +0800

    (feature): add new component for notification add form

---
+++ client/src/app/views/admin/send-notifications/notification-form/notification-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-form',
+  imports: [],
+  templateUrl: './notification-form.html',
+  styleUrl: './notification-form.css',
+})
+export class NotificationForm {}
