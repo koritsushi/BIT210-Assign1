14361a3dc9c38747674d91e0fdf2349d4dbece3d
QUJIAWEI-SS <b2400595@helplive.edu.my>
2026-04-22 13:24:29 +0800

    change back

--- client/src/app/views/login/login.ts
+++ client/src/app/views/login/login.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
 import { AuthService } from '../../services/auth.services';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -59,9 +58,9 @@ export class Login {
                 else if (this.authService.getRole() === "Admin")
                     this.router.navigate(['./admin/dashboard']);
             },
-            error: (error: HttpErrorResponse) => {
-                this.error = error.error?.message || 'Login failed. Please try again.';
-            }
+           error: () => {
+                this.error = 'Invalid Email or password!';
+           }
         })
     }
 
